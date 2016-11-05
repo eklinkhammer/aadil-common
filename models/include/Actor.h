bf4c386bd76f293e054c1240b13affbf05ee581d
Eric Klinkhammer <klinkhae@10-249-91-189.wireless.oregonstate.edu>
2016-11-04 17:10:33 -0700

    Adding models

---
+++ models/include/Actor.h
@@ -0,0 +1,56 @@
+/*********************************************************************
+*  actor.h
+*
+*  Actor is an interface for all agents / pois / other changing components
+*    of world in a simulation.
+*
+*  Copyright (C) 2016 Eric Klinkhammer
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
+#include <vector>
+
+#include "fann.h"
+#include "fann_cpp.h"
+
+#include "location.h"
+
+class Actor {
+ public:
+  virtual void move();
+
+  /*
+    Calculates the reward (may be difference, global, local, etc) of an actor, given a set of other actors
+
+    Args:
+      All actors visible to this actor.
+      G - the global reward signal
+   */
+  virtual double determineReward (std::vector<Actor*>&,double);
+
+  /*
+    If applicable, do something with the global reward. Anticipated use case: update local approximations
+      of G.
+
+    Args:
+      Broadcast value of G.
+   */
+  virtual void receiveBroadcastG(double);
+
+  Location getLocation();
+  
+ private:
+  Location loc;
+};
---
+++ models/src/location.cpp
@@ -0,0 +1,45 @@
+/*********************************************************************
+*  location.cpp
+*
+*  Location stores a double tuple, plus some static useful operations on
+*    locations.
+*
+*  Copyright (C) 2016 Eric Klinkhammer
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
+#include "location.h"
+
+Location::Location(double x, double y) {
+  this->x = x;
+  this->y = y;
+}
+
+Location Location::createLoc(double x, double y) {
+  Location loc(x,y);
+  return loc;
+}
+
+Location Location::addLocations(Location* loc1, Location* loc2) {
+  double newX = loc1->x + loc2->x;
+  double newY = loc1->y + loc2->y;
+  return createLoc(newX, newY);
+}
+
+double Location::distance(Location* loc1, Location* loc2) {
+  double diffX = loc2->x - loc1->x;
+  double diffY = loc2->y - loc1->y;
+  return sqrt(diffX*diffX + diffY*diffY);
+}
