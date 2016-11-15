4c4b33414aec145f8e030a095a18c3507a669f3f
Eric Klinkhammer <klinkhae@10-249-91-189.wireless.oregonstate.edu>
2016-11-15 03:34:53 -0800

    Corrected local D++. Global D++ has same logic error.

--- models/src/localAgentDpp.cpp
+++ models/src/localAgentDpp.cpp
@@ -36,7 +36,10 @@ LocalAgentDpp::LocalAgentDpp(Location loc, FANN::neural_net* startingNet) : Loca
 }
 
 // Estimates global reward if agent had done one less step
-double LocalAgentDpp::determineReward(std::vector<Actor*>& actors, double g) {
+double LocalAgentDpp::determineReward(std::vector<Actor*>& actors, double G) {
+  double newDpp = 0;
+  Location loc = this->getLocation();
+  Location previousCommand = this->getLastCommand();
   std::vector<double> state = this->createState(actors);
   state.push_back(0);
   state.push_back(0);
@@ -48,16 +51,31 @@ double LocalAgentDpp::determineReward(std::vector<Actor*>& actors, double g) {
   
   fann_type* output = this->getApproximation()->run(input);
   double D = (double) ((float) output[0]);
-  
+
+  if (G - D > 0) {
+    return G - D;
+  }
   std::vector<Actor*> actorCopy;
   for (const auto actor : actors) {
     actorCopy.push_back(actor);
   }
+  state = this->createState(actorCopy);
+  state.push_back(previousCommand.x);
+  state.push_back(previousCommand.y);
+  for (int i = 0; i < 10; i++) {
+    input[i] = (float) state[i];
+  }
 
-  Location loc = this->getLocation();
-  Location previousCommand = this->getLastCommand();
+  output = this->getApproximation()->run(input);
+  double counter = (double) ((float) output[0]);
 
-  double Dpp = 0;
+  double Dpp = counter - G;
+  if (Dpp > 0) {
+    return Dpp;
+  }
+  
+
+
   // TODO parametarize this value
   for (int i = 1; i < 10; i++) {
     Agent a (Location::createLoc(loc.x + 0.05, loc.y + 0.05));
@@ -69,13 +87,15 @@ double LocalAgentDpp::determineReward(std::vector<Actor*>& actors, double g) {
       input[i] = (float) state[i];
     }
 
-    fann_type* output = this->getApproximation()->run(input);
-    double counter = (double) ((float) output[0]);
-    Dpp += (counter / (i + 1));
+    output = this->getApproximation()->run(input);
+    counter = (double) ((float) output[0]);
+    newDpp = (counter - G) / i;
+    if (newDpp > Dpp) {
+      return newDpp;
+    } else {
+      Dpp = newDpp;
+    }
   }
 
-  double score1 = g - D;
-  double score2 = g - Dpp;
-  
-  return (score1 > score2 || score1 > 0) ? score1 : score2;
+  return newDpp;
 }
