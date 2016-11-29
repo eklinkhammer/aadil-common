35090d0dbb70dd8ebd12b7f57eab62df9e05c876
Eric Klinkhammer <klinkhae@10-249-91-189.wireless.oregonstate.edu>
2016-11-28 17:05:22 -0800

    Case Change

---
+++ models/include/localAgentDpp.h
@@ -0,0 +1,38 @@
+/*********************************************************************
+*  localAgentDpp.h
+*
+*  Local D++
+*
+*  Copyright (C) 2016 Eric Klinkhammer
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
+#ifndef _LOCALAGENTDPP_H
+#define _LOCALAGENTDPP_H
+
+#include <vector>
+
+#include "localagent.h"
+
+class LocalAgentDpp : public LocalAgent {
+ public:
+  double determineReward(std::vector<Actor*>&,double);
+  
+  LocalAgentDpp();
+  LocalAgentDpp(Location);
+  LocalAgentDpp(Location,FANN::neural_net*);
+};
+
+#endif
